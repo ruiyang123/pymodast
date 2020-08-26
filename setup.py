c7acd0460bbd86e26cc10e40c46ede6841608037
ruiyang123 <47784085+ruiyang123@users.noreply.github.com>
2020-08-26 15:37:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+from setuptools import setup
+
+setup(name='pymodast',
+      version='0.1',
+      description='Meta modeling tools for hydraulic models',
+      url='http://github.com/ruiyang123/pymodast',
+      author='yang123',
+      author_email='rui.yang.ecl@gmail.com',
+      license='MIT',
+      packages=['pymodast'],
+      zip_safe=False)
 
